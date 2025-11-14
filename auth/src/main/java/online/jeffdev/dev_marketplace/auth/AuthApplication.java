9e203c367a0de946104e398c76da2a8c32552ebf
Jefferson Villa <jeffersonvillacortes@gmail.com>
2025-11-14 16:58:41 -0500

    add auth microservice generated by spring initializr

---
+++ auth/src/main/java/online/jeffdev/dev_marketplace/auth/AuthApplication.java
@@ -0,0 +1,13 @@
+package online.jeffdev.dev_marketplace.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthApplication.class, args);
+	}
+
+}
